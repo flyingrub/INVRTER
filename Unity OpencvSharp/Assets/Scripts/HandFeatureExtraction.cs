4c5a5dd81edc176e1b903a7455aad18ed67e020a
flyingrub <flyingrub@protonmail.com>
2018-05-01 19:49:39 +0200

    Compute rough hull

--- Unity OpencvSharp/Assets/Scripts/HandFeatureExtraction.cs
+++ Unity OpencvSharp/Assets/Scripts/HandFeatureExtraction.cs
@@ -1,11 +1,15 @@
 using OpenCvSharp;
 using System.Collections;
+using System.Collections.Generic;
+
 using System;
+using System.Linq;
 
 public class HandFeatureExtraction {
 
 	Point[] contour;
-	Point[] hull;
+	IndexedPoint[] hull;
+	Point[] defects;
 	Mat mask;
 
 	public Mat getMask() {
@@ -23,6 +27,7 @@ public class HandFeatureExtraction {
 		mask = handColor.getMask(image);
 		contour = getContours(mask);
 		hull = getConvexHull(contour);
+		defects = getDefects();
 	}
 
 	public Point[] getContours(Mat image) {
@@ -72,21 +77,82 @@ public class HandFeatureExtraction {
 		return image;
 	}
 
+	public int[] getHullIndices() {
+		return hull.Select((iPoint) => iPoint.indice).ToArray();
+	}
+
+	public Point[] getHullPoints() {
+		return hull.Select((iPoint) => iPoint.p).ToArray();
+	}
+
 	public Mat getFeature(Mat image) {
 		image = drawContour(image, contour, Scalar.Blue);
-		image = drawContour(image, hull, Scalar.Red);
+		image = drawPoints(image, getHullPoints());
+		image = drawContour(image, getHullPoints(), Scalar.Red);
 		return image;
 	}
 
-	public Point[] getConvexHull(Point[] contour) {
-		return Cv2.ConvexHull(contour, false);
+	public Mat drawPoints(Mat image, Point[] points) {
+		foreach (Point p in points) {
+			image.Circle(p, 10, Scalar.Red);
+		}
+		return image;
+	}
+
+	public IndexedPoint[] getConvexHull(Point[] contour) {
+		var hullIndices = Cv2.ConvexHullIndices(contour, false);
+		return removeTooClose(hullIndices).ToArray();
+	}
+
+	public List<IndexedPoint> removeTooClose(int[] hullIndices) {
+		List<IndexedPoint> res = new List<IndexedPoint>();
+		var indexedPoints = hullIndices.Select((indice) => new IndexedPoint{ indice = indice, p = contour[indice] });
+		var clusters = indexedPoints.GroupBy(i => i.p, new ClosenessComparer(40));
+		foreach (var group in clusters)
+		{
+			var list = group.ToList();
+			int median = list.Count()/2;
+			res.Add(list[median]);
+		}
+		return res;
+	}
+
+	public Point[] getDefects() {
+		int[] hullIndices = getHullIndices();
+		var defects = Cv2.ConvexityDefects(contour, hullIndices);
+		return null;
 	}
 
 	public float getHandArea() {
 		if (contour == null) {
 			return 0;
 		}
-		var boundingRect = Cv2.BoundingRect(contour); //Find bounding rect for each contour
+		var boundingRect = Cv2.BoundingRect(contour);
 		return boundingRect.Width * boundingRect.Height;
+	}
+}
+
+public class IndexedPoint {
+	public Point p;
+	public int indice;
+}
+
+public class ClosenessComparer : IEqualityComparer<Point>
+{
+	private readonly int delta;
+
+	public ClosenessComparer(int delta)
+	{
+		this.delta = delta;
+	}
+
+	public bool Equals(Point a, Point b)
+	{
+		return Point.Distance(a, b) < delta;
+	}
+
+	public int GetHashCode(Point point)
+	{
+		return 0;
 	}
 }
